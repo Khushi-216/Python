6c5fb9fe9fca0bf8bd09414b2ba9b38b973250f7
Khushi-216 <shikhu216@gmail.com>
2025-01-25 22:26:37 +0000

    modified:   prac1A.py 	modified:   prac1B.py

--- prac1A.py
+++ prac1A.py
@@ -3,8 +3,10 @@
 
 l1=[[1,2,3],[4,5,6],[7,8,9]]
 l2=[[9,8,7],[6,5,4],[3,2,1]] 
-l3=[[],[],[]] 
-for i in range (0,len(l1)): 
+l3=[] 
+for i in range (0,len(l1)):
+    temp=[]
     for j in range(0,len(l1[i])): 
-        l3[i].append(l1[i][j]+l2[i][j]) 
+        temp.append(l1[i][j]+l2[i][j]) 
+    l3.append(temp)
 print(l3)
